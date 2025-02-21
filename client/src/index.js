261ded2682fbe981793ecc0075cbd5e8ffa862b8
ansshul10 <ansshul10@gmail.com>
2025-02-22 00:12:23 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import { RouterProvider } from 'react-router-dom';
-import router from './rotues'; // Fix the typo (should be routes, not rotues)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />  {/* RouterProvider should not wrap App */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();
