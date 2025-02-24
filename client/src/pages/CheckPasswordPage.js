7f8cd769f56f98d161ad8158a68ce3dd1b90a17b
ansshul10 <ansshul10@gmail.com>
2025-02-24 20:24:05 +0530

    Update CheckPasswordPage.js

--- client/src/pages/CheckPasswordPage.js
+++ client/src/pages/CheckPasswordPage.js
@@ -1,50 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import { IoClose } from "react-icons/io5";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import uploadFile from '../helpers/uploadFile';
+import axios from 'axios'
 import toast from 'react-hot-toast';
+import { PiUserCircle } from "react-icons/pi";
 import Avatar from '../components/Avatar';
 import { useDispatch } from 'react-redux';
-import { setToken } from '../redux/userSlice';
+import { setToken, setUser } from '../redux/userSlice';
 
 const CheckPasswordPage = () => {
-  const [data, setData] = useState({ password: "" });
-  const navigate = useNavigate();
-  const location = useLocation();
-  const dispatch = useDispatch();
+  const [data,setData] = useState({
+    password : "",
+    userId : ""
+  })
+  const navigate = useNavigate()
+  const location = useLocation()
+  const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (!location?.state?.name) {
-      navigate('/email');
+  useEffect(()=>{
+    if(!location?.state?.name){
+      navigate('/email')
     }
-  }, [location, navigate]);
+  },[])
 
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    setData((prev) => ({ ...prev, [name]: value }));
-  };
+  const handleOnChange = (e)=>{
+    const { name, value} = e.target
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/password`;
+    setData((preve)=>{
+      return{
+          ...preve,
+          [name] : value
+      }
+    })
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    e.stopPropagation()
+
+    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/password`
 
     try {
-      const response = await axios.post(URL, { 
-        userId: location?.state?._id, 
-        password: data.password 
-      }, { withCredentials: true });
+        const response = await axios({
+          method :'post',
+          url : URL,
+          data : {
+            userId : location?.state?._id,
+            password : data.password
+          },
+          withCredentials : true
+        })
 
-      toast.success(response.data.message);
+        toast.success(response.data.message)
 
-      if (response.data.success) {
-        dispatch(setToken(response?.data?.token));
-        localStorage.setItem('token', response?.data?.token);
-        setData({ password: "" });
-        navigate('/');
-      }
+        if(response.data.success){
+            dispatch(setToken(response?.data?.token))
+            localStorage.setItem('token',response?.data?.token)
+
+            setData({
+              password : "",
+            })
+            navigate('/')
+        }
     } catch (error) {
-      toast.error(error?.response?.data?.message);
+        toast.error(error?.response?.data?.message)
     }
-  };
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black to-gray-900 p-4">
